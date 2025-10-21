493b11dc972bcddcd7ab60f9a8d17cdf08cc3307
hein-01 <142292126+hein-01@users.noreply.github.com>
2025-10-21 05:59:44 +0000

    Delete all files and folders

--- supabase/migrations/20250927010831_42f8f295-00bc-4ec3-92de-f562a940cffd.sql
+++
@@ -1,3 +0,0 @@
--- Add info_website column to businesses table
-ALTER TABLE public.businesses 
-ADD COLUMN info_website text;
--- supabase/migrations/20251007135712_69aae632-dab1-4f5e-b9ed-762fce64e578.sql
+++
@@ -1,3 +0,0 @@
--- Add searchable_business column to businesses table
-ALTER TABLE public.businesses 
-ADD COLUMN searchable_business BOOLEAN NOT NULL DEFAULT TRUE;
